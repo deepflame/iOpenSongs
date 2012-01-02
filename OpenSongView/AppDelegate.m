1aa133a3cc622953804be1a5dd479abfdefdb354
Andreas Boehrnsen <aboehrnsen@openit.com>
2012-01-02 10:21:02 +0800

    FIX: Popoverview for portrait orientation

--- OpenSongView/AppDelegate.m
+++ OpenSongView/AppDelegate.m
@@ -15,6 +15,9 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    UISplitViewController *splitViewController = (UISplitViewController *)self.window.rootViewController;
+    UINavigationController *navigationController = [splitViewController.viewControllers lastObject];
+    splitViewController.delegate = (id)navigationController.topViewController;
     return YES;
 }
 							
