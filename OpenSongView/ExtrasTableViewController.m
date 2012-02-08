0a7fb4d9ae0bc51a916c2eb8e349b1d693b8188c
Andreas Boehrnsen <aboehrnsen@openit.com>
2012-02-09 07:54:02 +0800

    Dev: added Feedback Mail

--- OpenSongView/ExtrasTableViewController.m
+++ OpenSongView/ExtrasTableViewController.m
@@ -8,8 +8,9 @@
 
 #import "ExtrasTableViewController.h"
 #import "HtmlViewController.h"
+#import <MessageUI/MessageUI.h>
 
-@interface ExtrasTableViewController ()
+@interface ExtrasTableViewController () <MFMailComposeViewControllerDelegate>
 {
     IBOutlet UISwitch *nightModeSwitch;
 }
@@ -22,42 +23,12 @@
 @synthesize nightModeEnabled = _nightModeEnabled;
 @synthesize delegate = _delegate;
 
-- (id)initWithStyle:(UITableViewStyle)style
-{
-    self = [super initWithStyle:style];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
-- (void)didReceiveMemoryWarning
-{
-    // Releases the view if it doesn't have a superview.
-    [super didReceiveMemoryWarning];
-    
-    // Release any cached data, images, etc that aren't in use.
-}
-
 #pragma mark - View lifecycle
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
- 
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;    
-}
 
 - (void)viewDidUnload
 {
     nightModeSwitch = nil;
     [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -66,27 +37,32 @@
     nightModeSwitch.on = self.nightModeEnabled;
 }
 
-- (void)viewDidAppear:(BOOL)animated
-{
-    [super viewDidAppear:animated];
-}
-
-- (void)viewWillDisappear:(BOOL)animated
-{
-    [super viewWillDisappear:animated];
-}
-
-- (void)viewDidDisappear:(BOOL)animated
-{
-    [super viewDidDisappear:animated];
-}
-
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-    // Return YES for supported orientations
 	return YES;
 }
 
+#pragma mark - UITableViewDelegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath 
+{
+    // TODO: select based on title
+    if (indexPath.section == 2 && indexPath.row == 0 && [MFMailComposeViewController canSendMail]) {
+        MFMailComposeViewController *mailViewController = [[MFMailComposeViewController alloc] init];
+        mailViewController.mailComposeDelegate = self;
+        [mailViewController setSubject:@"[iOpenSongs] Feedback"];
+        [mailViewController setToRecipients:[NSArray arrayWithObject:@"iOpenSongs@boehrnsen.de"]];
+        [self presentModalViewController:mailViewController animated:YES];
+    }
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+#pragma mark - MFMailComposeViewControllerDelegate
+                                                 
+- (void)mailComposeController:(MFMailComposeViewController*)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError*)error{
+    [self dismissModalViewControllerAnimated:YES];
+}
+                                                 
 #pragma mark - Segues
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
