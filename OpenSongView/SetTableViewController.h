13ef5224e44d0dd03a68c4276e84d7e6321eaec5
Andreas Boehrnsen <andreas@boehrnsen.de>
2012-05-12 13:17:06 +0800

    implement UI mockup for Set

---
+++ OpenSongView/SetTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SetTableViewController.h
+//  iOpenSongs
+//
+//  Created by Andreas Böhrnsen on 5/12/12.
+//  Copyright (c) 2012 Andreas Boehrnsen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SetTableViewController : UITableViewController
+
+@end
---
+++ OpenSongView/SetTableViewController.m
@@ -0,0 +1,144 @@
+//
+//  SetTableViewController.m
+//  iOpenSongs
+//
+//  Created by Andreas Böhrnsen on 5/12/12.
+//  Copyright (c) 2012 Andreas Boehrnsen. All rights reserved.
+//
+
+#import "SetTableViewController.h"
+
+@interface SetTableViewController ()
+
+@end
+
+@implementation SetTableViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // 0: add set, 1: set list
+    return 2;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    // 0: add set, 1: set list
+    if (section == 0) {
+        return 1;
+    }
+    return 4;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // 0: add set, 1: set list
+    if (indexPath.section == 0) {
+        static NSString *CellIdentifier = @"New Set Cell";
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+        
+        if (cell == nil) {
+            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle
+                                          reuseIdentifier:CellIdentifier];
+        }
+        
+        return cell;  
+    } else {
+        static NSString *CellIdentifier = @"Set Cell";
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+        
+        if (cell == nil) {
+            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle
+                                          reuseIdentifier:CellIdentifier];
+        }
+        
+        //Song *song = [self.fetchedResultsController objectAtIndexPath:indexPath];
+        //cell.textLabel.text = song.title;
+        //cell.detailTextLabel.text = song.author;
+        
+        cell.textLabel.text = [NSString stringWithFormat:@"Sample Set %d", indexPath.row];
+        cell.detailTextLabel.text = @"0 Songs";
+        
+        return cell;
+    }
+    
+}
+
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // 0: add set, 1: set list
+    if (indexPath.section == 0) {
+        return NO;
+    } else {
+        return YES;
+    }
+}
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }   
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Navigation logic may go here. Create and push another view controller.
+    /*
+     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
+     // ...
+     // Pass the selected object to the new view controller.
+     [self.navigationController pushViewController:detailViewController animated:YES];
+     */
+}
+
+@end
