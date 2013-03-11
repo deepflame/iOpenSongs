04a02dc568a887916d9ea1092c4e40878494ebe6
Andreas Boehrnsen <andreas@boehrnsen.de>
2013-03-11 19:20:35 +0800

    use manual migrations

--- Classes/StartupViewController.m
+++ Classes/StartupViewController.m
@@ -15,6 +15,8 @@
 
 @interface StartupViewController ()
 
+@property (nonatomic, assign) BOOL isMigratingInBackground;
+
 @end
 
 @implementation StartupViewController
@@ -34,7 +36,9 @@
     
     [self setupCoreData];
     
-    //[self showMainUI]; // will be called from 'setupCoreData'
+    if (!self.isMigratingInBackground) {
+        [self showMainUI];
+    }
 }
 
 - (void)didReceiveMemoryWarning
@@ -58,9 +62,10 @@
     
     // return if no update needed
     if (songsNeedUpdate.count == 0) {
-        [self showMainUI];
         return;
     }
+    
+    self.isMigratingInBackground = YES;
     
     MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
     hud.mode = MBProgressHUDModeAnnularDeterminate;
@@ -96,12 +101,118 @@
 - (void) setupCoreData
 {
     NSString *coreDataStoreFileName = [MagicalRecord defaultStoreName];
+    NSURL *storeURL = [NSPersistentStore MR_urlForStoreName:coreDataStoreFileName];
     
-    [self moveDatabaseToMRStoreName:coreDataStoreFileName];
+    // if database not found we may have to move it
+    if (![[NSFileManager defaultManager] isReadableFileAtPath:[storeURL path]]) {
+        [self moveDatabaseToMRStoreName:coreDataStoreFileName];
+        
+        // if still not found initialize CoreData and return (new database)
+        if (![[NSFileManager defaultManager] isReadableFileAtPath:[storeURL path]]) {
+            [MagicalRecord setupCoreDataStackWithStoreNamed:coreDataStoreFileName];
+            return;
+        }
+    }    
     
-    [MagicalRecord setupCoreDataStackWithAutoMigratingSqliteStoreNamed:coreDataStoreFileName];
+    // we have a datastore and will check if we have to migrate it
+    
+    NSError *error = nil;
+    [self migrateDataStore:storeURL error:&error];
+    
+    [MagicalRecord setupCoreDataStackWithStoreNamed:coreDataStoreFileName];
     
     [self initializeSongTitleSectionIndex];
+}
+
+- (BOOL) migrateDataStore:(NSURL *)storeURL error:(NSError **)error
+{
+    NSDictionary *storeMetadata = [NSPersistentStoreCoordinator metadataForPersistentStoreOfType:NSSQLiteStoreType
+                                                                                             URL:storeURL
+                                                                                           error:error];
+    // TODO: Do error checking...
+    
+    BOOL success = NO;
+    
+    
+    if ([self storeMetaData:storeMetadata isVersion:3]) {
+        success = YES; // nothing to do
+    }
+    
+    else if ([self storeMetaData:storeMetadata isVersion:2]) {
+        success = [self migrateStore:storeURL
+                      fromModelNamed:@"iOpenSongs 2.mom"
+                        toModelNamed:@"iOpenSongs 3.mom" error:error];
+    }
+    
+    else if ([self storeMetaData:storeMetadata isVersion:1]) {
+        success = [self migrateStore:storeURL
+                      fromModelNamed:@"iOpenSongs 1.mom"
+                        toModelNamed:@"iOpenSongs 3.mom" error:error];
+    }
+    
+    return success;
+}
+
+- (BOOL)storeMetaData:(NSDictionary *)metaData isVersion:(NSUInteger)version
+{
+    NSString *modelName = [NSString stringWithFormat:@"iOpenSongs %u.mom", version];
+    NSManagedObjectModel *model = [NSManagedObjectModel MR_newModelNamed:modelName inBundleNamed:@"iOpenSongs.momd"];
+    NSPersistentStoreCoordinator *psc = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:model];
+    
+    NSManagedObjectModel *destinationModel = [psc managedObjectModel];
+    BOOL pscCompatible = [destinationModel isConfiguration:nil compatibleWithStoreMetadata:metaData];
+    
+    return pscCompatible;
+}
+
+
+- (BOOL)migrateStore:(NSURL *)storeURL fromModelNamed:(NSString *)srcModelName toModelNamed:(NSString *)dstModelName error:(NSError **)error {
+
+    NSManagedObjectModel *srcModel = [NSManagedObjectModel MR_newModelNamed:srcModelName inBundleNamed:@"iOpenSongs.momd"];
+    NSManagedObjectModel *dstModel = [NSManagedObjectModel MR_newModelNamed:dstModelName inBundleNamed:@"iOpenSongs.momd"];
+
+    NSMappingModel *mappingModel = [NSMappingModel inferredMappingModelForSourceModel:srcModel
+                                                                     destinationModel:dstModel
+                                                                                error:error];
+    // return if we cannot map the models
+    if (!mappingModel) {
+        return NO;
+    }
+
+    NSURL *dstStoreURL = [storeURL URLByAppendingPathExtension:@"tmp"];
+    
+    NSFileManager *fileMan = [NSFileManager defaultManager];
+    if ([fileMan fileExistsAtPath:[dstStoreURL path]]) {
+        [fileMan removeItemAtURL:dstStoreURL error:error];
+    }
+    
+    NSMigrationManager *manager = [[NSMigrationManager alloc] initWithSourceModel:srcModel destinationModel:dstModel];
+    
+    // migrate
+    BOOL success = [manager migrateStoreFromURL:storeURL
+                                           type:NSSQLiteStoreType
+                                        options:nil
+                               withMappingModel:mappingModel
+                               toDestinationURL:dstStoreURL
+                                destinationType:NSSQLiteStoreType
+                             destinationOptions:nil
+                                          error:error];
+    
+    // move migrated database
+    if (success) {        
+        NSURL *oldStoreURL = [storeURL URLByAppendingPathExtension:@"old"];
+        
+        if ([fileMan fileExistsAtPath:[oldStoreURL path]]) {
+            [fileMan removeItemAtURL:oldStoreURL error:error];
+        }
+        
+        // backup last version
+        success = [fileMan moveItemAtURL:storeURL toURL:oldStoreURL error:error];
+        // set new version
+        success = [fileMan moveItemAtURL:dstStoreURL toURL:storeURL error:error];
+    }
+    
+    return success;
 }
 
 /** migrates/moves the database to the new MR location */
