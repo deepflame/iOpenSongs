0cd2a1712cea7811c9a63ca4c13d15e4496bd06b
Andreas Boehrnsen <andreas@boehrnsen.de>
2013-03-13 23:16:33 +0800

    fixed section header sorting

--- Classes/SongTableViewController.m
+++ Classes/SongTableViewController.m
@@ -36,11 +36,11 @@
     }
     
     // fetch data
-    self.fetchedResultsController = [Song MR_fetchAllSortedBy:@"titleNormalized"
-                                                    ascending:YES
-                                                withPredicate:nil
-                                                      groupBy:@"titleSectionIndex"
-                                                     delegate:self];
+    self.fetchedResultsController = [Song MR_fetchAllGroupedBy:@"titleSectionIndex"
+                                                 withPredicate:nil
+                                                      sortedBy:@"titleSectionIndex,titleNormalized"
+                                                     ascending:YES
+                                                      delegate:self];
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
