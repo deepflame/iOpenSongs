c6bc75561080f5cf569175b386f40a538b1c3e6a
Andreas Boehrnsen <andreas@boehrnsen.de>
2013-03-14 18:51:43 +0800

    added category for document dir access

---
+++ Classes/UIApplication+Directories.h
@@ -0,0 +1,15 @@
+//
+//  UIApplication+Directories.h
+//  iOpenSongs
+//
+//  Created by Andreas Böhrnsen on 3/14/13.
+//  Copyright (c) 2013 Andreas Boehrnsen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIApplication (Directories)
+
++ (NSString *)documentsDirectory;
+
+@end
---
+++ Classes/UIApplication+Directories.m
@@ -0,0 +1,18 @@
+//
+//  UIApplication+Directories.m
+//  iOpenSongs
+//
+//  Created by Andreas Böhrnsen on 3/14/13.
+//  Copyright (c) 2013 Andreas Boehrnsen. All rights reserved.
+//
+
+#import "UIApplication+Directories.h"
+
+@implementation UIApplication (Directories)
+
++ (NSString *)documentsDirectory
+{
+	return [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+}
+
+@end
