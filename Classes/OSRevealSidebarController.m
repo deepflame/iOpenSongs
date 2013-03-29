6a64c47968bdec56a0df5c33280ef0a218b688d7
Andreas Boehrnsen <andreas@boehrnsen.de>
2013-03-29 19:28:13 +0100

    create tabbar in code

--- Classes/OSRevealSidebarController.m
+++ Classes/OSRevealSidebarController.m
@@ -18,8 +18,30 @@
 {
     [super viewDidLoad];
 
+    // build tabbar
+    UIViewController *songsViewController = [self.storyboard instantiateViewControllerWithIdentifier:@"songs"];
+    UIViewController *setsViewController = [self.storyboard instantiateViewControllerWithIdentifier:@"sets"];
+    UIViewController *settingsViewController = [self.storyboard instantiateViewControllerWithIdentifier:@"settings"];
+    
+    UITabBarItem *songsTBI = [[UITabBarItem alloc] initWithTitle:@"Songs" image:[UIImage imageNamed:@"glyphicons_017_music"] tag:0];
+    UITabBarItem *setsTBI = [[UITabBarItem alloc] initWithTitle:@"Sets" image:[UIImage imageNamed:@"glyphicons_158_playlist"] tag:1];
+    UITabBarItem *settingsTBI = [[UITabBarItem alloc] initWithTitle:@"Settings" image:[UIImage imageNamed:@"glyphicons_023_cogwheels"] tag:2];
+    
+    songsViewController.tabBarItem = songsTBI;
+    setsViewController.tabBarItem = setsTBI;
+    settingsViewController.tabBarItem = settingsTBI;
+    
+    NSMutableArray *viewControllers = [NSMutableArray array];
+    [viewControllers addObject:[[UINavigationController alloc] initWithRootViewController:songsViewController]];
+    [viewControllers addObject:[[UINavigationController alloc] initWithRootViewController:setsViewController]];
+    [viewControllers addObject:[[UINavigationController alloc] initWithRootViewController:settingsViewController]];
+
+    UITabBarController *tabBarController = [[UITabBarController alloc] init];
+    tabBarController.viewControllers = viewControllers;
+
+    // setup sidebar controller
     self.topViewController = [[self storyboard] instantiateViewControllerWithIdentifier:@"TopView"];
-    self.underLeftViewController = [[self storyboard] instantiateViewControllerWithIdentifier:@"Sidebar"];
+    self.underLeftViewController = tabBarController;
 
     if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
         self.anchorRightRevealAmount = 305.0;
