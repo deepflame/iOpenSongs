dfdce373a92451c78a38f6e71e17a493100679c3
Andreas Boehrnsen <andreas@boehrnsen.de>
2013-03-31 22:57:10 +0200

    use barbuttonitem to dismiss about

--- Classes/OSSupportTableViewController.m
+++ Classes/OSSupportTableViewController.m
@@ -166,35 +166,19 @@
     if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
         [self.navigationController pushViewController:aboutVC animated:YES];
     } else {
-        // modal view
+        // bar button to dismiss modal view
+        UIBarButtonItem *doneBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Close" style:UIBarButtonItemStylePlain handler:^(id sender) {
+            [self dismissModalViewControllerAnimated:YES];
+        }];
+        aboutVC.navigationItem.leftBarButtonItem = doneBarButtonItem;
+        
+        // configure modal view
         UINavigationController *aboutNC = [[UINavigationController alloc] initWithRootViewController:aboutVC];
         aboutNC.modalPresentationStyle = UIModalPresentationFormSheet;
         aboutNC.modalTransitionStyle = UIModalTransitionStyleCrossDissolve;
         
-        // add tap gesture to dismiss the modal view
-        UITapGestureRecognizer *recognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTapBehind:)];
-        [recognizer setNumberOfTapsRequired:1];
-        recognizer.cancelsTouchesInView = NO; //So the user can still interact with controls in the modal view
-        [self.view.window addGestureRecognizer:recognizer];
-        
         [self.delegate dismissSupportPopoverAnimated:YES];
         [self presentModalViewController:aboutNC animated:YES];
-    }
-}
-
-// thanks to Danilo Campos and junglecat
-// http://stackoverflow.com/questions/2623417/iphone-sdk-dismissing-modal-viewcontrollers-on-ipad-by-clicking-outside-of-it
-- (void)handleTapBehind:(UITapGestureRecognizer *)sender
-{
-    if (sender.state == UIGestureRecognizerStateEnded) {
-        CGPoint location = [sender locationInView:nil]; //Passing nil gives us coordinates in the window
-        
-        //Then we convert the tap's location into the local view's coordinate system, and test to see if it's in or outside. If outside, dismiss the view.
-        if (![self.view pointInside:[self.view convertPoint:location fromView:self.view.window] withEvent:nil]) {
-            // Remove the recognizer first so it's view.window is valid.
-            [self.view.window removeGestureRecognizer:sender];
-            [self dismissModalViewControllerAnimated:YES];
-        }
     }
 }
 
