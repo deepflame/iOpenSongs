9a9841add70fe8648d09cfa6a282727ea366cf33
Andreas Böhrnsen <andreas@boehrnsen.de>
2013-05-26 14:25:56 +0800

    add initial test controller for KIF

---
+++ Testing/OSTestController.m
@@ -0,0 +1,24 @@
+//
+//  OSTestController.m
+//  iOpenSongs
+//
+//  Created by Andreas Böhrnsen on 5/26/13.
+//  Copyright (c) 2013 Andreas Boehrnsen. All rights reserved.
+//
+
+#import "OSTestController.h"
+
+@implementation OSTestController
+
+- (void)initializeScenarios;
+{
+    // If your app is doing anything interesting with parameterized scenarios,
+    // you'll want to override this method and add them manually.
+    //    [self addScenario:[KIFTestScenario scenarioToLogin]];
+    //    [self addScenario:[KIFTestScenario scenarioToSelectDifferentColors]];
+    
+    // If you're not,
+    [super initializeScenarios];
+}
+
+@end
